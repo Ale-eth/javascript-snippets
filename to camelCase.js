583e0f2341d0ea8d1d3f7a8a43fbce01559c4592
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-04-23 23:01:26 -0300

    Create to camelCase.js

---
+++ to camelCase.js
@@ -0,0 +1,8 @@
+const toCamelCase = (str) =>
+  str.trim().replace(/[-_\s]+(.)?/g, (_, c) => (c ?
+    c.toUpperCase() : ''));
+
+toCamelCase('background-color'); // backgroundColor
+toCamelCase('-webkit-scrollbar-thumb'); // WebkitScrollbarThumb 
+toCamelCase('_hello_world'); // HelloWorld 
+toCamelCase('hello_world'); //  helloWorld 
